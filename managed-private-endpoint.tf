0ab4f44b6972646fd2987cfce27d3db260de172c
Alex Bance <Alex.Bance@justice.gov.uk>
2023-10-06 15:07:38 +0100

    feat: updated managed private endpoint to handle nulls

--- managed-private-endpoint.tf
+++ managed-private-endpoint.tf
@@ -1,13 +1,5 @@
-resource "azurerm_data_factory_managed_private_endpoint" "purview" {
-  count              = var.purview_id != "" && var.purview_id != null ? 1 : 0
-  name               = "${local.name}-purview-${var.env}"
-  data_factory_id    = azurerm_data_factory.this.id
-  target_resource_id = var.purview_id
-  subresource_name   = "account"
-}
-
 resource "azurerm_data_factory_managed_private_endpoint" "this" {
-  for_each           = var.managed_private_endpoints
+  for_each           = { for key, value in var.managed_private_endpoints : key => value if value.resource_id != null }
   name               = "${local.name}-${each.key}-${var.env}"
   data_factory_id    = azurerm_data_factory.this.id
   target_resource_id = each.value.resource_id
