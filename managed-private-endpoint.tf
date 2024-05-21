85f73ba83b7b8bac46fbc7e85b98d8c1642bf5a4
Alex Bance <Alex.Bance@justice.gov.uk>
2024-05-21 13:51:59 +0100

    fix: separate out private endpoint approvals

--- managed-private-endpoint.tf
+++ managed-private-endpoint.tf
@@ -25,7 +25,7 @@ resource "azurerm_data_factory_managed_private_endpoint" "dependants" {
 }
 
 resource "null_resource" "private_endpoint_approvals" {
-  for_each = var.managed_private_endpoints
+  for_each = { for key, value in var.managed_private_endpoints : key => value if value.is_managed_resource == false }
 
   provisioner "local-exec" {
     interpreter = ["/bin/bash", "-c"]
@@ -45,11 +45,38 @@ resource "null_resource" "private_endpoint_approvals" {
   }
   depends_on = [
     azurerm_data_factory_managed_private_endpoint.this,
-    azurerm_data_factory_managed_private_endpoint.dependants
   ]
   lifecycle {
     replace_triggered_by = [
       azurerm_data_factory_managed_private_endpoint.this,
+    ]
+  }
+}
+
+resource "null_resource" "dependant_private_endpoint_approvals" {
+  for_each = { for key, value in var.managed_private_endpoints : key => value if value.is_managed_resource == true }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<-EOT
+                  az account set --subscription ${data.azurerm_subscription.current.subscription_id}
+                  endpoints=$(az network private-endpoint-connection list --id ${each.value.resource_id} --query "[?starts_with(not_null(properties.privateLinkServiceConnectionState.description, ''), 'Requested by DataFactory') && properties.privateLinkServiceConnectionState.status == 'Pending'].id" --output json)
+                  endpointsLength=$(echo "$endpoints" | jq "length - 1")
+                  if [ $endpointsLength -lt 0 ]; then
+                    echo "No pending private endpoint connections found."
+                  else
+                    for i in $(seq 0 $endpointsLength); do
+                      endpoint=$(echo "$endpoints" | jq -r ".[$i]")
+                      az network private-endpoint-connection approve --id $endpoint --description "Approved by Terraform via local-exec provisioner."
+                    done
+                  fi
+    EOT
+  }
+  depends_on = [
+    azurerm_data_factory_managed_private_endpoint.dependants
+  ]
+  lifecycle {
+    replace_triggered_by = [
       azurerm_data_factory_managed_private_endpoint.dependants
     ]
   }
