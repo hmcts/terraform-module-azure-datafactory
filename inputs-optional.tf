f438d00bf6def4fdb04528c578590e665b59659c
Narayn Manoharan <narayn.manoharan@justice.gov.uk>
2024-09-27 14:13:40 +0100

    Adding GitHub Configuration Support for Module

--- inputs-optional.tf
+++ inputs-optional.tf
@@ -158,3 +158,14 @@ variable "global_parameters" {
   }))
   default = {}
 }
+
+variable "github_configuration" {
+  description = "Map of GitHub configuration settings for the Azure Data Factory."
+  type = map(object({
+    branch_name        = string
+    git_url            = string
+    repository_name    = string
+    root_folder        = string
+    publishing_enabled = bool
+  }))
+}
